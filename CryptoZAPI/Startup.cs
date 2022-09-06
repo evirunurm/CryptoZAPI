ac456e956d9edb0ae5e541c7c41b0ea41b316c54
Alejandro Calderón García <alejandrocgmas@gmail.com>
2022-09-06 09:26:23 +0200

    errors behavior

--- CryptoZAPI/Startup.cs
+++ CryptoZAPI/Startup.cs
@@ -1,20 +1,69 @@
 ﻿using CryptoZAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Models;
 using Models.Mappers;
 using NomixServices;
 using Repo;
 using RestCountriesServices;
 
-public static class Startup {
+public static class Startup
+{
 
-    public static void ConfigureServices(this WebApplicationBuilder builder) {
+    public static void ConfigureServices(this WebApplicationBuilder builder)
+    {
 
         // AutoMapper
         //builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         builder.Services.AddAutoMapper(typeof(CurrencyProfile), typeof(HistoryProfile), typeof(UserProfile), typeof(CountryProfile));
 
         // Controllers
-        builder.Services.AddControllers().AddXmlDataContractSerializerFormatters();
+        builder.Services.AddControllers()
+            .AddXmlDataContractSerializerFormatters().
+            ConfigureApiBehaviorOptions(setupAction =>
+            {
+                setupAction.InvalidModelStateResponseFactory = context =>
+                {
+                    // create a problem details objectk
+                    var problemDetailsFactory = context.HttpContext.RequestServices
+                        .GetRequiredService<ProblemDetailsFactory>();
+                    var problemDetails = problemDetailsFactory.CreateValidationProblemDetails(
+                            context.HttpContext,
+                            context.ModelState);
+
+                    // add additional info not added by default
+                    problemDetails.Detail = "See the errors field for details.";
+                    problemDetails.Instance = context.HttpContext.Request.Path;
+
+                    // find out which status code to use
+                    var actionExecutingContext =
+                        context as Microsoft.AspNetCore.Mvc.Filters.ActionExecutingContext;
+
+                    // if there are modelstate errors & all keys were correctly
+                    // found/parsed we're dealing with validation errors
+                    if ((context.ModelState.ErrorCount > 0) &&
+                        (actionExecutingContext?.ActionArguments.Count == context.ActionDescriptor.Parameters.Count))
+                    {
+                        problemDetails.Type = "https://courselibrary.com/modelvalidationproblem";
+                        problemDetails.Status = StatusCodes.Status422UnprocessableEntity;
+                        problemDetails.Title = "One or more validation errors occurred.";
+
+                        return new UnprocessableEntityObjectResult(problemDetails)
+                        {
+                            ContentTypes = { "application/problem+json" }
+                        };
+                    }
+
+                    // if one of the keys wasn't correctly found / couldn't be parsed
+                    // we're dealing with null/unparsable input
+                    problemDetails.Status = StatusCodes.Status400BadRequest;
+                    problemDetails.Title = "One or more errors on input occurred.";
+                    return new BadRequestObjectResult(problemDetails)
+                    {
+                        ContentTypes = { "application/problem+json" }
+                    };
+                };
+            });
 
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
@@ -31,7 +80,8 @@ public static class Startup {
 
 
         // Nomics
-        builder.Services.AddHttpClient<INomics, Nomics>(client => {
+        builder.Services.AddHttpClient<INomics, Nomics>(client =>
+        {
             client.BaseAddress = new Uri("https://api.nomics.com/v1/");
         });
 
@@ -42,8 +92,10 @@ public static class Startup {
         });
     }
 
-    public static void Configure(this WebApplication app) {
-        if (app.Environment.IsDevelopment()) {
+    public static void Configure(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
             app.UseSwagger();
             app.UseSwaggerUI();
             app.UseDeveloperExceptionPage();
